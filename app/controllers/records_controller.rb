27ec3e6242fd96ccf1ae7e73bee83bf5b4dbd09b
StrugglingOwl <brucelou@live.com>
2017-05-13 22:22:52 +0800

    check user permission when access edit/update/destroy

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,5 @@
 class RecordsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @records = Record.all
   end
