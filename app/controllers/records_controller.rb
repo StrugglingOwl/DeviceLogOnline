cd910e1df6f9ccf1904d973ce4e63666d3b6bcbe
StrugglingOwl <brucelou@live.com>
2017-05-13 12:32:49 +0800

    implement records#create

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,4 +6,18 @@ class RecordsController < ApplicationController
   def new
     @record = Record.new
   end
+
+  def create
+    @record = Record.new(record_params)
+    @record.save
+
+      redirect_to records_path
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:date, :room_id, :device_name, :device_model, :serial_number, :summary, :resolution, :result, :remarks)
+
+  end
 end
