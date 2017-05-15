1923bf40bb69771fe62111e28ec8b6f6e7fee289
StrugglingOwl <brucelou@live.com>
2017-05-15 11:53:25 +0800

    use self delimit helper

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,10 @@
 module RecordsHelper
+  def render_record_summary(record)
+    simple_format(record.summary)
+  end
+
+  def render_record_resolution(record)
+    simple_format(record.resolution)
+  end
+
 end
